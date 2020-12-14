dbe8e2cc68fa8a5ff24439a7b2b56c0b13aebed6
Kamil Burdziński <kamil.burdzinski@student.put.poznan.pl>
2020-12-14 20:17:28 +0100

    Fix children problem

--- node.py
+++ node.py
@@ -2,10 +2,10 @@ import numpy as np
 
 
 class Node:
-    def __init__(self, fun, id_, children=[]):
+    def __init__(self, fun, id_):
         self.fun = fun
         self.id = id_
-        self.children = children
+        self.children = []
         self.val = None
 
     def __call__(self, W):
@@ -14,4 +14,4 @@ class Node:
         return self.val
     
     def __repr__(self):
-        return f'{self.id}: {self.fun} | {self.val}'
+        return f'{self.id}: {self.fun} | {self.val:0.3f}'
